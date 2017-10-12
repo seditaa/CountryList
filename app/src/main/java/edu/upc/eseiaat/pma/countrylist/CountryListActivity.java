1b124a3b17825d6884524c1f009e01c9ca95d8cc
seditaa <daniel.martinez.g1@estudiant.upc.edu>
2017-10-12 22:29:08 +0200

    short click on list

--- app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/countrylist/CountryListActivity.java
@@ -2,8 +2,11 @@ package edu.upc.eseiaat.pma.countrylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -23,7 +26,19 @@ public class CountryListActivity extends AppCompatActivity {
 
         ListView list = (ListView) findViewById(R.id.country_list);
 
+        //Adaptadot
+
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, country_list);
         list.setAdapter(adapter);
+
+        //Toque rápido en un país
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View item, int pos, long id) {
+                String short_click = "Has escogigo " + country_list.get(pos) + ".";
+                Toast.makeText(CountryListActivity.this, short_click, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
